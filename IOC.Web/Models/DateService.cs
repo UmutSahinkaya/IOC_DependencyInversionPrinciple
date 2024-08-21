8ff1b066613ec033e536afe752899692e73505c6
umutSahinkaya <umutsahinkaya1@gmail.com>
2024-08-21 16:56:27 +0300

    Daha iyi anlaşılması için MVC uygulamasına geçiş yaptım

---
+++ IOC.Web/Models/DateService.cs
@@ -0,0 +1,15 @@
+﻿
+namespace IOC.Web.Models;
+
+public class DateService : ISingletonDateService, IScopedDateService, ITransientDateService
+{
+    private readonly ILogger<DateService> _logger;
+
+    public DateService(ILogger<DateService> logger)
+    {
+        _logger = logger;
+        _logger.LogWarning("DateService ctor'a girdi.");
+    }
+
+    public DateTime GetDateTime { get; } = DateTime.Now;
+}
